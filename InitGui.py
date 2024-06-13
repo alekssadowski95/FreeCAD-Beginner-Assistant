e8835108ed6958c8879e01e983bee464790e0b53
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-06-13 12:00:48 -0500

    Move path_to_mod import into Workbench init function

--- InitGui.py
+++ InitGui.py
@@ -19,15 +19,13 @@ import os
 import FreeCAD # noqa
 import FreeCADGui # noqa
 
-from config import path_to_mod
-
-
 class UselessWorkbench(FreeCADGui.Workbench):
     """Purpose and functionality of the UselessWorkbench."""
 
     def __init__(self):
         self.__class__.MenuText = "FreeCAD Beginner Assistant"
         self.__class__.ToolTip = "A description of the FreeCAD Beginner Assistant"
+        from config import path_to_mod
         self.__class__.Icon = os.path.join(path_to_mod, 'FreeCAD-Beginner-Assistant', 'icons', 'owl-2.png')
 
     def Initialize(self):
