e661ee46fc0c9fe98921fec0940593ab5f38ac62
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-06-13 00:16:54 -0500

    Remove Hello World code example

--- InitGui.py
+++ InitGui.py
@@ -19,18 +19,6 @@ import os
 import FreeCAD # noqa
 import FreeCADGui # noqa
 
-# Print normal text to "Report View" console 
-print("Print: Hello, World!")
-
-# Print normal text to "Report View" console 
-FreeCAD.Console.PrintMessage("Message: Hello, World!" + "\n")
-
-# Print orange Warning text to "Report View" console 
-FreeCAD.Console.PrintWarning("Warning: Hello, World!" + "\n")
-
-# Print red error text to "Report View" console 
-FreeCAD.Console.PrintError("Error: Hello, World!" + "\n")
-
 class UselessWorkbench(FreeCADGui.Workbench):
     """Purpose and functionality of the UselessWorkbench."""
 
