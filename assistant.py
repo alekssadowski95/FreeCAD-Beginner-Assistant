d41316cfb214dd85be15687e9eed7cf05e7a5a74
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2024-10-02 11:19:01 +0200

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -42,10 +42,19 @@ def get_under_constrained_sketches(doc: FreeCAD.Document):
     return under_constrained_sketches
     
 def has_under_constrained_sketch(doc: FreeCAD.Document):
+    result = {
+        "id" : 2,
+        "action" : "You have created a sketch, that is under constrained.",
+        "effect" : "This might lead to unexpected behaviour, when you use that sketch for a feature.",
+        "solution" : "Go back to your sketch and fully define it using dimensional of geometrical constraints.",
+        "status" : "Unchecked"
+    }
     if len(get_under_constrained_sketches(doc)) > 0:
-        return True
+        result['status'] = "Failed"
+        return result
     else:
-        return False
+        result['status'] = "Passed"
+        return result
 
 # ID 3: You have created a sketch, that is over constrained.
 def get_over_constrained_sketches(doc: FreeCAD.Document):
@@ -58,10 +67,19 @@ def get_over_constrained_sketches(doc: FreeCAD.Document):
     return over_constrained_sketches
 
 def has_over_constrained_sketch(doc: FreeCAD.Document):
+    result = {
+        "id" : 3,
+        "action" : "You have created a sketch, that is over constrained.",
+        "effect" : "This might lead to unexpected behaviour, when you use that sketch for a feature.",
+        "solution" : "Go back to your sketch and remove redundant constraints.",
+        "status" : "Unchecked"
+    }
     if len(get_over_constrained_sketches(doc)) > 0:
-        return True
+        result['status'] = "Failed"
+        return result
     else:
-        return False
+        result['status'] = "Passed"
+        return result
 
 # ID 4: Your 3D model is not symmetric in relation to one of the Origin planes.
 
@@ -313,62 +331,37 @@ def generate_result_dict(fcstd_file_path=""):
         "date" : str(today),
         "file" : filename,
         "screenshot" : "C:\\Users\\Aleksander\\Documents\\GitHub\\FreeCAD-Beginner-Assistant\\model_images\\test.png",
-        "pts-reached" : "17",
-        "pts-available" : "21",
+        "pts-reached" : "not available",
+        "pts-available" : "not available",
         "rank" : "Bronze Apprentice",
         "best-practices" : []
     }
 
     result_dict["best-practices"].append(
-        {
-        "id" : 1,
-        "action" : "You have referenced a face of your 3D model (topological element) for your sketch.",
-        "effect" : "This might lead to the sketch losing its reference, when the topological elements change.",
-        "solution" : "Reference one of the Origin planes or create a new plane, that also only references one of the Origin planes instead.",
-        "status" : "Passed"
-        }
+        has_over_constrained_sketch(FreeCAD.ActiveDocument())
     )
 
     result_dict["best-practices"].append(
-        {
-        "id" : 2,
-        "action" : "You have created a sketch, that is under constrained",
-        "effect" : "This might lead to unexpected behaviour, when you use that sketch for a feature",
-        "solution" : "Go back to your sketch and fully define it using dimensional of geometrical constraints.",
-        "status" : "Passed"
-        }
+        has_under_constrained_sketch(FreeCAD.ActiveDocument())
     )
 
-    result_dict["best-practices"].append(
-        {
-        "id" : 3,
-        "action" : "You have created a sketch, that is over constrained",
-        "effect" : "This might lead to unexpected behaviour, when you use that sketch for a feature",
-        "solution" : "Go back to your sketch and remove redundant constraints.",
-        "status" : "Passed"
-        }
-    )
+    # Populate points fields in result dict
+    pts_reached = 0
+    pts_available = 0
 
-    result_dict["best-practices"].append(
-        {
-        "id" : 4,
-        "action" : "Your 3D model is not symmetric in relation to one of the Origin planes",
-        "effect" : "Designing your 3D model symmetric to as many Origin planes as possible makes it easier to modify it in the future.",
-        "solution" : "Try to create your 3D model symmetric in relation to as many Origin planes as possible.",
-        "status" : "Passed"
-        }
-    )
+    for practice in result_dict["best-practices"]:
+        if practice['status'] == "Passed":
+            pts_available = pts_available + 1
+            pts_reached = pts_reached + 1
+        elif practice['status'] == "Failed":
+            pts_available = pts_available + 1
+        else:
+            pass
 
-    result_dict["best-practices"].append(
-        {
-        "id" : 5,
-        "action" : "You have created a complex sketch that uses a lot of geometrical elements and constraints.",
-        "effect" : "This might lead to performance issues and make building your 3D model slow.",
-        "solution" : "Split up your complex sketch into multiple simple sketches if possible.",
-        "status" : "Passed"
-        }
-    )
+    result_dict["pts-reached"] = pts_reached
+    result_dict["pts-available"] = pts_available
 
+        
     return result_dict
 
 
